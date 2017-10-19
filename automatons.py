f34e0b625fe3c02e0ee152f08224a4763d354dc4
Rodrigo <rodrigo.redcode@gmail.com>
2017-10-19 11:23:31 -0200

    Generic automata

---
+++ automatons.py
@@ -0,0 +1,36 @@
+sigma = {
+    ("Q0", "0"): {"Q1"}, 
+    ("Q1", "0"): {"Q2"},
+    ("Q2", "0"): {"Q3"},
+    ("Q2", "1"): {"Q4"},
+    ("Q3", "0"): {"Q2"},
+    ("Q4", "1"): {"Q2"},
+}
+
+
+def make_automaton(initial_state, sigma, valid_states):
+    
+    def automaton(string):
+        states = {initial_state}
+        for c in string:
+            new_states = set()
+            for state in states:
+                try:
+                    new_states.update(sigma[state, c])
+                except KeyError:
+                    return False
+            states = new_states
+
+        return any(state in valid_states for state in states)
+    
+    return automaton
+
+
+s = "0011001100111111"
+valid_states = {"Q2"}
+
+
+zero_one_automata = make_automaton("Q0", sigma, valid_states)
+
+print(zero_one_automata("000011"))
+print(zero_one_automata("0000111"))
